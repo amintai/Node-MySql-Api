be715c032951764834a32a7df89df0882a427e24
Amin Tai <amintai157@gmail.com>
2021-05-13 13:58:36 +0530

    Stored Procedure

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const moment = require('moment')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const { response } = require('express')
 
 const app = express()
 const PORT = 5000
@@ -24,11 +26,25 @@ db.getConnection((err)=> {
       console.log(`Database Connected`)
 })
 
+// calling procedure 
+
+app.get('/api/procedure/filterStudent' , (req,res) => {
+
+    db.query('CALL filterStudent(true)',(err,result) => {
+        if(!err) {
+            console.log(result[0])
+            res.send(result)
+        } else {
+            console.log(err.message)
+        }
+    })
+})
 
 //get all employees
 app.get('/' , (req,res) => {
 
-    db.query('SELECT * FROM employee', (err,result) => {
+    // let date = "DATE_FORMAT(dob,  '%M %e, %Y')"
+    db.query(`SELECT name , email  , sub_name  FROM student , subject  where student.id = subject.std_id `, (err,result) => {
 
         if(!err){
             res.send(result)
@@ -37,10 +53,26 @@ app.get('/' , (req,res) => {
         }
     })
 })
+
+// add subjects 
+app.post('/api/subject',(req,res) => {
+    db.query(`insert into subject(sub_name) values ('${req.body.sub_name}')`, (err,result) => {
+        if(!err){
+             res.send(`Subject ${req.body.sub_name}  Added`)
+        } else {
+            console.log(err.message)
+        }
+    })
+})
 // add record 
 app.post('/api/create',(req,res) => {
-    db.query(`insert into employee(name,email,age) values('${req.body.name}','${req.body.email}','${req.body.age}')`,(err,result) => {
-    
+
+    let date = req.body.dob
+    console.log(date)
+
+    let dob = Date.prototype.toLocaleDateString(date)
+    db.query(`insert into employee(name,email,age,dob) values ('${req.body.name}','${req.body.email}','${req.body.age}' , '${dob}')`,(err,result) => {
+        
         if(!err) {
             res.send(`Employee with name: ${req.body.name} has been added`)
         } else {
@@ -79,7 +111,7 @@ app.delete('/api/delete/:id', (req,res) => {
 // update
 app.put('/api/update/:id',(req,res) => {
     console.log(req.params.id)
-    db.query(`UPDATE employee SET name='${req.body.name}' , email='${req.body.email}' , age= ${req.body.age} WHERE  id = ${req.params.id}` , (err,result) => {
+    db.query(`UPDATE employee SET name='${req.body.name}' , email='${req.body.email}' , age= ${req.body.age}, dob = '${req.body.dob}' WHERE  id = ${req.params.id}` , (err,result) => {
 
         if(!err) {
             res.send(`Details updated where id = ${req.params.id} `)
